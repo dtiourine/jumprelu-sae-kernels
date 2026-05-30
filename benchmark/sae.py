b35f1bd46de4b668ec7150ef4984b5aebf3af67e
DTiourine <109561478+DTiourine@users.noreply.github.com>
2026-05-30 17:11:05 -0400

    Add additional wrapper to match SAE lens decode method contract

---
+++ benchmark/sae.py
@@ -0,0 +1,3 @@
+from sae_lens import SAE 
+
+sae = SAE.from_pretrained("sae-lens/sparse-jumprelu-sae-1b-128d")
--- src/kernel_jumprelu_sae/wrapper.py
+++ src/kernel_jumprelu_sae/wrapper.py
@@ -3,16 +3,20 @@ import triton
 import torch
 
 
-def sparse_decode(idx, val, W_dec, BLOCK_D=256):
+def _sparse_decode(idx, val, W_dec, BLOCK_D=256):
     d_model = W_dec.shape[1]
     L0 = idx.shape[0]
 
     out = torch.zeros(d_model, device=W_dec.device, dtype=torch.float32)
 
     grid = (triton.cdiv(d_model, BLOCK_D),)
-    
-    sparse_decode_kernel[grid](
-        idx, val, W_dec, out, L0, d_model, BLOCK_D=BLOCK_D
-    )
-    
-    return out 
+
+    sparse_decode_kernel[grid](idx, val, W_dec, out, L0, d_model, BLOCK_D=BLOCK_D)
+
+    return out
+
+
+def sparse_decode(feature_acts, W_dec):
+    idx = feature_acts.nonzero().squeeze(-1).to(torch.int32)
+    val = feature_acts[idx]
+    return _sparse_decode(idx, val, W_dec)
